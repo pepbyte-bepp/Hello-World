cf6d8d0a69feaab5b6a7dc6076052307e2b6fe6a
pepbyte-bepp <rubyeast0529@gmail.com>
2026-09-03 14:47:58 -0700

    Add files via upload

---
+++ CS110-Dome-Signal-Starter/script.js
@@ -0,0 +1,10 @@
+const signalButton = document.querySelector("#signal-button");
+const domeStatus = document.querySelector("#dome-status");
+ 
+signalButton.addEventListener("click", function (){
+ domestatus.textContent = "Kairova Status: Online";
+});
+
+// TODO: Add a click event listener.
+// When the learner presses the button, change domeStatus.textContent so the
+// next crew can confirm the dome is online or read its next instruction.
